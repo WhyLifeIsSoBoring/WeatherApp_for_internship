840b7116f88bbe8ba23eaa5e7f7946953f55e421
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-17 17:41:47 +0300

    fix url

--- lib/services/current_weather_api_provider.dart
+++ lib/services/current_weather_api_provider.dart
@@ -9,7 +9,7 @@ class CurrentWeatherProvider {
     String lat = "53.893009";   
     String lon = "27.567444";
     String apiKey = "fa9d791964704e17c3d2943c20e2533a";
-    var url = "api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=$apiKey";
+    var url = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=$apiKey&units=metric";
 
     final response  = await http.get(Uri.parse(url));
 

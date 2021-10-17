40ffb5ce13eff3b4e5f922815bab69487e4e0d43
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-17 17:44:42 +0300

    do MainPage with using bloc

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_for_internship/bloc/weather_bloc.dart';
+import 'package:weather_app_for_internship/bloc/weather_event.dart';
+import 'package:weather_app_for_internship/bloc/weather_state.dart';
 import 'package:weather_app_for_internship/presentation/custom_icons_icons.dart';
+import 'package:weather_app_for_internship/services/current_weather_api_provider.dart';
+import 'package:weather_app_for_internship/widgets/main_current_weather_view.dart';
 
-
-class MainPage  extends StatefulWidget {
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
+class MainPage extends StatelessWidget {
+  CurrentWeatherProvider currentWeatherProvider = CurrentWeatherProvider();
   List<Widget> _widgetOptions = <Widget>[
+    //MainCurrentWeatherView(currentWeatherProvider),
     Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -29,25 +29,37 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_currentIndex),
-      bottomNavigationBar: BottomNavigationBar (
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(CustomIcons.sun),
-            label: 'Today',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CustomIcons.cloud_moon),
-            label: 'Forecast',
-            backgroundColor: Colors.blue,
-          ),
-        ],
-      ),
+    return BlocProvider<WeatherBloc>(
+      create: (context) => WeatherBloc(currentWeatherProvider)..add(CurrentWeatherLoadEvent()),
+      child: BlocBuilder<WeatherBloc, WeatherState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: _widgetOptions.elementAt(state.currentIndex),
+            bottomNavigationBar: BottomNavigationBar (
+              type: BottomNavigationBarType.fixed,
+              currentIndex: state.currentIndex,
+              onTap: (index)  {
+                if(index == 0) {
+                  BlocProvider.of<WeatherBloc>(context).add(CurrentWeatherLoadEvent());
+                } else if (index == 1) {
+                  BlocProvider.of<WeatherBloc>(context).add(ForecastWeatherLoadEvent());
+                }
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(CustomIcons.sun),
+                  label: 'Today',
+                  backgroundColor: Colors.blue,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CustomIcons.cloud_moon),
+                  label: 'Forecast',
+                  backgroundColor: Colors.blue,
+                ),
+              ],
+            ),
+          );
+        })
     );
   }
 }

3e4972d1408a25361c7d7ead93f29584c4794410
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-17 17:41:04 +0300

    add states for forecast and add in all states field currentIndex

--- lib/bloc/weather_state.dart
+++ lib/bloc/weather_state.dart
@@ -1,15 +1,33 @@
 import 'package:weather_app_for_internship/models/current_weather.dart';
 
-abstract class WeatherState {}
+abstract class WeatherState {
+  late int currentIndex;
+  //get currentIndex => null;
+}
 
-class CurrentWeatherInitialState extends WeatherState{}
-
-class CurrentWeatherLoadingState extends WeatherState {}
+class CurrentWeatherLoadingState extends WeatherState {
+  final int currentIndex = 0;
+}
 
 class CurrentWeatherLoadedState extends WeatherState {
+  final int currentIndex = 0;
   CurrentWeather currentWeather;
   CurrentWeatherLoadedState({required this.currentWeather});
 }
 
-class CurrentWeatherErrorState extends WeatherState {}
+class CurrentWeatherErrorState extends WeatherState {
+  final int currentIndex = 0;
+}
 
+class ForecastWeatherLoadingState extends WeatherState {
+  final int currentIndex = 1;
+}
+
+class ForecastWeatherLoadedState extends WeatherState {
+  final int currentIndex = 1;
+
+}
+
+class ForecastWeatherErrorState extends WeatherState {
+  final int currentIndex = 1;
+}

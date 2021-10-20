e03a9513dccce3da99491b083fa236dd1e629ad1
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-20 14:03:34 +0300

    add new model weather_every_3h for forcast and model weather with current weather and forecast

---
+++ lib/models/weather_every_3h.dart
@@ -0,0 +1,8 @@
+class Weather3h {
+  final int dt;
+  final double temp;
+  final String description;
+  final String icon;
+
+  Weather3h({required this.dt, required this.temp, required this.description, required this.icon,});
+}

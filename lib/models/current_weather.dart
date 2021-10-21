5e7d76f4bb250804e9ca617548bb26920045c7cd
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-21 12:24:22 +0300

    add in model field timezone and update convert timestamps functions

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -1,6 +1,7 @@
 class CurrentWeather {
   final String city;
   final String country;
+  final int timezone;
   final int temp;
   final String main;
   final String icon;
@@ -11,11 +12,12 @@ class CurrentWeather {
   final int windDeg;
 
 
-  CurrentWeather({required this.city, required this.country, required this.temp, required this.main, required this.icon, required this.pop, required this.humidity, required this.pressure, required this.windSpeed, required this.windDeg});
+  CurrentWeather({required this.city, required this.country, required this.timezone, required this.temp, required this.main, required this.icon, required this.pop, required this.humidity, required this.pressure, required this.windSpeed, required this.windDeg});
   factory CurrentWeather.fromJson(Map<String, dynamic> json) {
     return CurrentWeather(
       city: json['city']['name'],
       country: json['city']['country'],
+      timezone: json['city']['timezone'],
       temp: (json['list'][0]['main']['temp']).toInt(),
       main: json['list'][0]['weather'][0]['main'],
       icon: json['list'][0]['weather'][0]['icon'],
